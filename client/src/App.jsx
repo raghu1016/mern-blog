b399a95a3709a9d08906799c65d060b472200999
raghu1016 <raghurukel1016@gmail.com>
2024-01-26 19:22:08 +0530

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,13 @@ import About from "./pages/About.jsx"
 import Dashboard from "./pages/Dashboard.jsx"
 import SignIn from "./pages/SignIn.jsx"
 import SignUp from "./pages/SignUp.jsx"
+import Projects from "./pages/Pojects.jsx"
+import Header from "./components/Header.jsx"
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
