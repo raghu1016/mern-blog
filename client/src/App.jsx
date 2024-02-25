57d62cd23a1971af084f622c8629d9b5f579c3ad
raghu1016 <raghurukel1016@gmail.com>
2024-02-25 20:16:41 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn/>}/>
         <Route path="/sign-up" element={<SignUp/>}/>
         <Route element={<PrivateRoute/>}>
-        <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
         </Route>        
         <Route path="/projects" element={<Projects/>}/>
       </Routes>
